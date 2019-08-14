660bc293422dcd469f3524ad539157babf5a62c9
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-08-13 20:42:17 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+var age=prompt("What is your age?");
+var age1 = age % 2;
+
+if (age < 0) {
+    console.error
+}   else if(age == 21) {
+    console.log("happy 21st birthday!!")
+}   else if(age1 !=0) {
+    console.log("your age is odd!")
+}   else if(age1 == 0) {
+    console.log("your age is even")
+}
